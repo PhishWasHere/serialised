3b3fd8cf2543feab7ee7c7da25efae9b926c186f
PhishWasHere <121412190+PhishWasHere@users.noreply.github.com>
2023-10-10 16:52:58 +1100

    comparison func woks

--- src/scraper/check-follow/index.ts
+++ src/scraper/check-follow/index.ts
@@ -9,14 +9,13 @@ export const getFollow = async (username: string, password: string) => {
     try{         
         await MD.login(username, password);
 
-        const mangaArr = (await MD.Manga.getFollowedManga()).map((manga) => ({
+        const mangaArr = (await MD.Manga.getFollowedManga((Infinity))).map((manga) => ({
             title: manga.title,
-            lastChapter: manga.lastChapter
+            latestChapter: manga.lastChapter
             })
         );
-
         const comparedArr = await compareChapter(mangaArr);
-
+            
     } catch (err) {
         const errMsg = getError(err);
         throw new Error(errMsg);
@@ -25,7 +24,7 @@ export const getFollow = async (username: string, password: string) => {
 
 type mangaArrType = {
     title: string,
-    lastChapter: string,
+    latestChapter: string | null,
 }[];
 
 type ManganatoManga = {
@@ -37,24 +36,71 @@ type ManganatoManga = {
     coverImage: string;
 }
  
-const compareChapter = async (manga: mangaArrType) => {
+const compareChapter = async (mangaArr: mangaArrType) => {
     try {
-        let errArr:string[] = [];
-        let returnArr:mangaArrType = [];
+        let errArr:string[] = []; // manga that couldnt be found, or contained some err (eg: no latest chapter)
+        let returnArr:mangaArrType = []; // manga that could be found
+        let resArr: object[] = []; // return object after comparison 
 
-        await Promise.all(manga.map(async (manga) => {
-            const res: ManganatoManga[] = await scraper.search(manga.title);
-            if (!res || res.length === 0) {
-                return errArr.push(manga.title);
-            }
-        
-            const scraperManga = (await scraper.getMangaMeta(res[0].url));
+
+        let test:any[] = [];
+        try {
+            await Promise.all(mangaArr.map(async (manga) => {
+                const res: ManganatoManga[] = await scraper.search(manga.title);
+    
+                if (!res || res.length === 0) {
+                    return errArr.push(manga.title);
+                }
             
-            const title = scraperManga.title.main;
-            const chapterName = scraperManga.chapters[0].name;
+                const scraperManga = (await scraper.getMangaMeta(res[0].url));
+
+                const title = scraperManga.title.main;
+                
+                if (scraperManga.chapters[0] && scraperManga.chapters[0].name) {
+                    const latestChapter = await getChapterNumber(scraperManga.chapters[0].name);
+                    if (!latestChapter) {
+                        return errArr.push(manga.title);
+                    }
+                    returnArr.push({ title, latestChapter });
+                } else {
+                    return errArr.push(manga.title);
+                }
+            }));        
+        } catch (err) {
+            const errMsg = getError(err);
+            throw new Error(errMsg);
+        }
+
+        try {
+            await Promise.all(returnArr.map(async (manga) => { 
+                switch (true) {
+                    case !manga.latestChapter || manga.latestChapter === '' || manga.latestChapter === null:
+                        errArr.push(manga.title);
+                        return
+                    break;
+    
+                    case manga.latestChapter != null && manga.latestChapter >= manga.latestChapter:
+                        resArr.push({title: manga.title, latestChapter: manga.latestChapter, success: true});
+                        return;
+                    break;
+    
+                    case manga.latestChapter != null && manga.latestChapter < manga.latestChapter:
+                        resArr.push({title: manga.title, latestChapter: manga.latestChapter, warn: true});
+                        return;
+                    break;
+    
+                    default:
+                        errArr.push(manga.title);
+                        return
+                    break;
+                }
+            }));
+        } catch (err) {
+            const errMsg = getError(err);
+            throw new Error(errMsg);
+        }
         
-            returnArr.push({title, lastChapter: chapterName});
-        }));
+        return {resArr, errArr};
 
     } catch (err) {
         const errMsg = getError(err);
---
+++ src/utils/fs-object-check/index.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import getError from '../get-error';
+
+export const fsCheck = async (index: object) => {
+    try {
+        const filePath = path.join(__dirname, 'followArr.json');
+        const content = JSON.stringify(index, null, 2);
+        fs.writeFile(filePath, content , 'utf8', (err) => {
+            if (err) {
+                const errMsg = getError(err);
+                throw new Error(errMsg);
+            }
+        });
+        console.log('File written successfully\n');
+    } catch (err) {
+        const errMsg = getError(err);
+        throw new Error(errMsg);
+    }
+}
+
